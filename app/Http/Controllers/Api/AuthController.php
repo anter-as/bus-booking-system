6dc53e8074ae1e46d6b80f0a224f049528eeb819
anter-as <aaa.ahmedanter@gmail.com>
2024-05-12 22:30:22 +0300

    Login api.

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 
@@ -27,6 +28,32 @@ class AuthController extends Controller
         }
 
         $user = User::create($request->all());
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken
+        ]);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation Error!',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Bad Credentials!'], 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
 
         return response()->json([
             'user' => $user,
