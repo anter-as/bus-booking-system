8839c7b40fcc7f9530afbaf4a7a16370f6eed740
anter-as <aaa.ahmedanter@gmail.com>
2024-05-13 01:50:14 +0300

    Add Trip model.

---
+++ database/migrations/2024_05_12_220653_create_trips_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 100);
+            $table->foreignId('bus_id');
+            $table->foreignId('start_station_id');
+            $table->foreignId('end_station_id');
+            $table->dateTime('starts_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
